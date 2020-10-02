336ea330bf855107fa60208768fbf585d13ce30a
BartoszCzernal <bartekczernal@gmail.com>
2020-10-02 10:56:03 +0200

    first commit

---
+++ firstChallenge/home.js
@@ -0,0 +1,16 @@
+function ageInDays() {
+  var birthYear = prompt("What year were you born?");
+  var ageInDays = (2020 - birthYear) * 365;
+  var h1 = document.createElement("h1");
+  var textAnswer = document.createTextNode(
+    "You are " + ageInDays + " days old"
+  );
+  h1.setAttribute("id", "ageInDays");
+  h1.appendChild(textAnswer);
+  document.getElementById("flex-box-result").appendChild(h1);
+  console.log(ageInDays);
+}
+
+function reset() {
+  document.getElementById("ageInDays").remove();
+}
